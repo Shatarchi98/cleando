fe6805f77e9c3946fa39ed8de7efb50c21a1a344
Shatarchi Goyal <34619375+Shatarchi98@users.noreply.github.com>
2023-09-19 15:42:17 +0530

    Update helper.js
    removed clientId and clientSecret

--- server/views/helper.js
+++ server/views/helper.js
@@ -1,7 +1,7 @@
 function oAuthdata(){
     
-    const CLIENT_ID='3MVG9G9pzCUSkzZuwNy2sUFMBRG8axKpXbPyl1JGFfXq08grG257CZEzFkJYQlLJNyri3aphfll4528doR71c';
-    const CLIENT_SECRET_ID='6F24875F2D704BF99AE8094B3D81CADC94E0EB28F9BD22D154F32504A70D2F3B'
+    const CLIENT_ID='';
+    const CLIENT_SECRET_ID=''
     const callbackPath = 'connectCont';
 
     return {CLIENT_ID, CLIENT_SECRET_ID, callbackPath};
